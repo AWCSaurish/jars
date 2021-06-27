4792b1890ad9070c268465ed29bb9a57abf5e2a9
Pratik Gaurav <pratik.gaurav@awcsoftware.com>
2021-06-27 15:10:58 +0530

    adding maven batch file

---
+++ mavencomands.bat
@@ -0,0 +1,8 @@
+mvn install:install-file -Dfile=D:\jars\nsms.jar -DgroupId=com.newgen.nsms -DartifactId=nsms -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\NIPLJ.jar -DgroupId=com.newgen.NIPLJ -DartifactId=NIPLJ -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\jtssessionbean.jar -DgroupId=com.newgen.jtssessionbean -DartifactId=jtssessionbean -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\jdts.jar -DgroupId=com.newgen.jdts -DartifactId=jdts -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\ISPack.jar -DgroupId=com.newgen.ispack -DartifactId=ispack -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\ejbclient.jar -DgroupId=com.newgen.ejbclient -DartifactId=ejbclient -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\dmsapi.jar -DgroupId=com.newgen.dmsapi -DartifactId=dmsapi -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\azure.jar -DgroupId=com.newgen.azure -DartifactId=azure -Dversion=1.0 -Dpackaging=jar
