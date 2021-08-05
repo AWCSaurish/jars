e362d47f71bab376e59152b4d8b6b071ad2388dd
Pratik Gaurav <pratik.gaurav@awcsoftware.com>
2021-08-05 10:52:41 +0530

    Added

--- mavencomands.bat
+++ mavencomands.bat
@@ -6,3 +6,5 @@ mvn install:install-file -Dfile=D:\jars\ISPack.jar -DgroupId=com.newgen.ispack -
 mvn install:install-file -Dfile=D:\jars\ejbclient.jar -DgroupId=com.newgen.ejbclient -DartifactId=ejbclient -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=D:\jars\dmsapi.jar -DgroupId=com.newgen.dmsapi -DartifactId=dmsapi -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=D:\jars\azure.jar -DgroupId=com.newgen.azure -DartifactId=azure -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\azure-storage-5.0.0.jar -DgroupId=com.newgen.azurestorage -DartifactId=azurestorage -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\jars\aws-java-sdk-1.11.40.jar -DgroupId=com.newgen.awssdk -DartifactId=awssdk -Dversion=1.0 -Dpackaging=jar
